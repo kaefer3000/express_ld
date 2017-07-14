d7674a72d7ac4591b960cb8d9d39d1dea5f5935d
Sebastian Lauber <dev@slauber.de>
2017-07-14 19:29:47 +0200

    Implemented the /seriousDataFormat GET endpoint
    Signed-off-by: Sebastian Lauber <dev@slauber.de>

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* we'll write proper js code */
+"use strict";
+
 const $rdf = require('rdflib');
 const jsonld = require('jsonld');
 const express = require('express');
@@ -16,8 +19,10 @@ const ACCEPTALL = "*/*";
 /* required RDF predefinitions */
 const LDP = $rdf.Namespace("http://www.w3.org/ns/ldp#");
 const RDF = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+const RDFS = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#");
+
 const LDPCONTAINER = LDP('BasicContainer');
-const RDFTYPE = RDF('type')
+const RDFTYPE = RDF('type');
 
 /* message store */
 let messages = []
@@ -26,11 +31,11 @@ let messages = []
 const buildRdf = (data, req, res) => {
     /* gets accept header */
     let serializationFormat = req.accepts()[0];
-    let store = $rdf.graph()
+    let store = $rdf.graph();
 
     /* send error, if no serialization format is set */
     console.log(serializationFormat);
-    if (!serializationFormat || serializationFormat == ACCEPTALL) {
+    if (!serializationFormat ||  serializationFormat == ACCEPTALL) {
         res.status(400).send("Set accept header, e.g. text/turtle application/rdf+xml application/ld+json");
     } else {
         let isJsonLd = false;
@@ -38,7 +43,7 @@ const buildRdf = (data, req, res) => {
         /* build base url dynamically */
         const baseUrl = req.protocol + "://" + req.hostname;
         const base = $rdf.sym(baseUrl);
-        const baseNs = $rdf.Namespace(baseUrl+req.path);
+        const baseNs = $rdf.Namespace(baseUrl + req.path);
 
         /* if only one object is to be displayed */
         if (Array.isArray(data)) {
@@ -47,14 +52,14 @@ const buildRdf = (data, req, res) => {
 
             /* inserts triples for each item of the store */
             if (data) {
-                data.forEach(function (message, idx) {
-                    store.add(base, LDP('contains'), baseNs(idx+1));
+                data.forEach(function(message, idx) {
+                    store.add(base, LDP('contains'), baseNs(idx + 1));
                 });
             }
         } else {
             store.add(data);
         }
-        
+
 
         /* jump through some hoops for JSON-LD */
         if (serializationFormat == JSONLD) {
@@ -65,11 +70,13 @@ const buildRdf = (data, req, res) => {
         /* serializes the data */
         $rdf.serialize(undefined, store, baseUrl, serializationFormat, (err, str) => {
             if (err) {
-                res.status(400).send(err+"\nSet accept header to supported format, e.g. text/turtle application/rdf+xml application/ld+json");
+                res.status(400).send(err + "\nSet accept header to supported format, e.g. text/turtle application/rdf+xml application/ld+json");
             } else {
                 /* use another transformation for JSON-LD */
                 if (isJsonLd) {
-                    jsonld.fromRDF(str, {format: 'application/nquads'}, function(err, doc) {
+                    jsonld.fromRDF(str, {
+                        format: 'application/nquads'
+                    }, function(err, doc) {
                         if (err) {
                             res.status(400).send(err);
                         } else {
@@ -77,17 +84,19 @@ const buildRdf = (data, req, res) => {
                         }
                     });
                 } else {
-                    res.send(str)
+                    res.send(str);
                 }
             }
         })
-                }
-            }
+    }
+}
 
 /**
-* parse everything as text (and try to get the best out of the libs)
-*/
-app.use(bodyParser.text({ type: ACCEPTALL }))
+ * parse everything as text (and try to get the best out of the libs)
+ */
+app.use(bodyParser.text({
+    type: ACCEPTALL
+}))
 
 /* returns an accordingly formatted response to the base object */
 app.get('/', (req, res) => {
@@ -95,25 +104,26 @@ app.get('/', (req, res) => {
 })
 
 /**
-* needs to receive the document and format it
-*/
+ * needs to receive the document and format it
+ */
 app.get('/:id', (req, res) => {
-    if (messages.length < req.params.id || req.params.id < 1) {
-        res.status(404).send("No document found with id "+req.params.id);
+    if (messages.length < req.params.id ||  req.params.id < 1) {
+        res.status(404).send("No document found with id " + req.params.id);
     } else {
-        buildRdf(messages[req.params.id-1], req, res)
-        //res.send(req.params.id);
+        buildRdf(messages[req.params.id - 1], req, res)
     }
 })
 
-/* does not support JSON-LD yet */
+/**
+ * does not support JSON-LD yet
+ */
 app.post('/', (req, res) => {
     let store = $rdf.graph();
     let mimeType = req.headers['content-type'];;
 
     let url = req.protocol + "://" + req.hostname;
     $rdf.parse(req.body, store, url, mimeType, (err, content) => {
-        if(err) {
+        if (err) {
             res.status(400).send(err);
             console.log("err", err);
         } else {
@@ -126,14 +136,35 @@ app.post('/', (req, res) => {
     });
 })
 
-/* clears the store */
+/**
+ * clears the store 
+ */
 app.delete('/', (req, res) => {
-    console.log("Deleting messages");
     messages = [];
-    res.json({success: true });
+    res.json({
+        success: true
+    });
 })
 
-/* makes the app listen on the selected port */
+/**
+ *   outputs the data for the display
+ */
+app.post('/getSeriousDataFormat', (req, res) => {
+    const matching = messages.filter((message) => message.statementsMatching(undefined, RDF("label"), undefined));
+    let output = [];
+    matching.map((message, idx) => output.push({
+        message: matching[0].statements[0].object.value,
+        index: idx
+    }));
+    res.json({
+        success: true,
+        output
+    });
+});
+
+/**
+ * makes the app listen on the selected port
+ */
 app.listen(httpPort, () => {
-    console.log('Turtle and JSON-LD receiver ready on port '+httpPort);
-})
+    console.log('Turtle and JSON-LD receiver ready on port ' + httpPort);
+});
